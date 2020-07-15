b0ca598348e7b3716bf0d572344ea0c19ab378d8
Paweł Miżwa <pmizwa@groupon.com>
2020-07-15 16:10:07 +0200

    error handling added to main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, make_response, request
 from flask_restful import Resource, Api
 from werkzeug.exceptions import BadRequestKeyError
 from flask_basicauth import BasicAuth
-from helpers.onp_handler import ONPHandler
+from helpers.onp_handler import ONPHandler, ONP_PHRASE_ERROR
 from config import prod
 import logging
 
@@ -37,6 +37,10 @@ class Data(Resource):
                 {"message": "Please specify phrase in params"}, 400
             )
             logger.warning(msg=e)
+        except ONP_PHRASE_ERROR as e:
+            response = make_response(
+                {"message": e}, 400
+            )
         except Exception as e:
             logger.critical(msg=e)
             response = make_response({"message": "System issue"}, 500)
